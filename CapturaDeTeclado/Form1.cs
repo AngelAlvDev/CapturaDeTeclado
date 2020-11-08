4ec208f47d1d2675e3e59fb7928e89d2669cefd2
AngelAlvAlumnCESCristoRey <angelalv.alumn@cescristorey.com>
2020-11-08 13:51:20 +0100

    Prueba de escritura en label

---
+++ CapturaDeTeclado/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapturaDeTeclado
+{
+    public partial class Form1 : Form
+    {
+        protected override bool ProcessCmdKey(ref System.Windows.Forms.Message msg, System.Windows.Forms.Keys keyData)
+        {
+
+            if (keyData == Keys.A)
+            {
+                label1.Text = label1.Text + "A";
+            }
+
+            if (keyData == Keys.B)
+            {
+                label1.Text = label1.Text + "B"; //aqui la accion del F2
+            }
+
+            return true;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
